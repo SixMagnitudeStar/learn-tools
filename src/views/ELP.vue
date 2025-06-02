cc1bc5562c154c3e28ad1ada0e3011275a70a27c
SixMagnitudeStar <95866947+SixMagnitudeStar@users.noreply.github.com>
2025-06-02 10:48:17 +0800

    20250602-1

--- src/views/ELP.vue
+++ src/views/ELP.vue
@@ -5,8 +5,21 @@
     </div>
 
     <div>
-      <input v-model="text" type="text" />
-      <button @click="speak">🔊 發音</button>
+      <input v-model="vocab" type="text" />
+      <button @click="appendVocab(vocab)">新增到聽力複習列表</button>
+      <button @click="speak">🔊 發音</button><span>{{listeningVocab }}</span>
+    </div>
+    <div>
+      <h2>聽力列表：</h2>
+      <ul>
+        <li v-for="(vocab, index) in vocabList" :key="index">{{vocab}}</li>
+      </ul>
+    </div>
+      <div>
+      <h2>聆聽列表：</h2>
+      <ul>
+        <li v-for="(vocab, index) in listeningList" :key="index">{{vocab}}</li>
+      </ul>
     </div>
   </template>
   
@@ -15,19 +28,45 @@
     name: 'ELPView',
     data() {
     return {
-      text: 'Hello, how are you?'
+      vocab: '',
+      vocabList: [],
+      listeningList: [],
+      listeningVocab: ''
     }
   },
   methods: {
     speak() {
+      
+      if (this.listeningList.length ===0){
+        alert('聆聽列表已空，請先新增詞彙');
+        return;
+      }
+
+
+      // 從聆聽列表中隨機取得索引
+      let randomIndex = Math.floor(Math.random() * this.listeningList.length);
+      
+      // 取得要聆聽的詞彙
+      // let randomWord = this.listeningList[randomIndex];
+      this.listeningVocab = this.listeningList[randomIndex];
+
+      // 將該詞彙從聆聽列表中移除
+      this.listeningList.splice(randomIndex, 1);
+      
+
       // 將要念的語音內容建構成一個物件
-      const utterance = new SpeechSynthesisUtterance(this.text);
+      const utterance = new SpeechSynthesisUtterance(this.listeningVocab);
 
       //指定語音的語言
       utterance.lang = 'en-US'; // 可改為 'zh-TW'、'ja-JP' 等
 
       //語音合成器物件呼叫speak method執行上面建立的語音內容物件
       speechSynthesis.speak(utterance);
+    },
+    appendVocab(vocab){
+      this.vocabList.push(vocab);
+      this.vocab = '';
+      this.listeningList = [...this.vocabList];
     }
   }
 
